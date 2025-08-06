b5ce6d7dea27e67c5dbc0ba5ae8afaf26acaf7f8
Heriyuru Venkata Sai Krishna <omnia771148@gmail.com>
2025-08-06 23:30:04 +0530

    kusha

--- src/app/mainRestorentList/page.js
+++ src/app/mainRestorentList/page.js
@@ -2,14 +2,14 @@
 
 import React from 'react';
 import RestorentList from '../restorentList/restorentList';
-import KushasMenuList from '../kushas/page';
+
 
 
 export default function Maindisplay(){
     return (<div>
         
     <RestorentList/>
-    <KushasMenuList/>
+  
 
     </div>
     )
