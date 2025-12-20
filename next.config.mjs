4f8431dba345afe6cfdf7eaf1b5b6c1a6ba51d44
unknown <y.parthureddy111@gmail.com>
2025-12-20 15:41:22 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-import withPWA from "next-pwa";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: false, // for localhost testing
-})(nextConfig);
+export default nextConfig;
